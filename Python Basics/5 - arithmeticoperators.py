55edba129874e226832a055aed498e194b791c8d
DuvanArwenLazar <3dtuningduvan@gmail.com>
2023-12-18 16:53:25 -0500

    Commit.

---
+++ Python Basics/5 - arithmeticoperators.py
@@ -0,0 +1,15 @@
+a, b = 2, 4
+# -- Sum, subtraction
+print(a + b)
+print(b - a)
+
+# Multiplication, division.
+print(a * b)
+print(b / a) # Return a float value.
+print(b // a) # Floor division (int value).
+
+# % Operator.
+print(b % a)
+
+# Exponentiation
+print(a ** b)
