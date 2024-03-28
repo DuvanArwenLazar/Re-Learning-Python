15c52a0beb570b5c1227019f0841a0677ceac071
DuvanArwenLazar <3dtuningduvan@gmail.com>
2024-03-28 17:39:00 -0500

    03/28/2024

---
+++ Python Basics/6 - comparisonoperators.py
@@ -0,0 +1,21 @@
+# Those operators returns a boolean value.
+
+# 1. Equal:
+print(5 == 3) # False.
+
+# 2. Greater than:
+print(5 > 3) # True.
+
+# 3. Less than.
+print(5 < 5) # False.
+
+# 4. Grater than or equal:
+print(10 >= 10) # True.
+
+# 5. Less than or equal:
+print(4 <= 10) # True.
+
+# 6. Diferent than:
+print(5 != 10) # True.
+# We'll use this operators for if statements and cicles.
+
---
+++ Python Basics/7 - booleanoperators.py
@@ -0,0 +1,10 @@
+# Boolean operators are use for compare boolean values.
+
+# 1. Not: Changes a True for a False and Changes a False for a True.
+print(not(True)) # False.
+
+# 2. And: Compares two boolean values. If both are True, "And" will be True. Else, "And" will be False.
+print(True and False) # False.
+
+# 3. Or: Compares two booleans values. Only needs one True value for will be True. If both bool values are False, "Or" will be False.
+print(True or False) # True.
